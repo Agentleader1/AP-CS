a2703809e9f60c91523bc1c65bd87461b8ba20cd
Alex C <agentleader1@gmail.com>
2021-03-25 14:45:13 -0700

    Made a basic HelloWorld start program
    For those who are starting out in AP-CS :)

---
+++ src/com/alex/apcsa/HelloWorld.java
@@ -0,0 +1,32 @@
+package com.alex.apcsa;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		println("Hello World!");
+		printf(true, "My name is %s, and this is my basic HelloWorld starter program file.", "Alex");
+		print("\nI hope you will find great help through this project repo.\nBut please do shoot me an email if you need help with AP-CS! lol");
+	}
+
+	public static void print(Object... objs) {
+		for (Object obj : objs) {
+			System.out.print(obj);
+		}
+	}
+	
+	public static void println(Object... objs) {
+		if (objs.length <= 0) {
+			System.out.println();
+		} else {
+			for (Object obj : objs) {
+				System.out.println(obj);
+			}
+		}
+	}
+	
+	public static void printf(boolean newLine, String format, Object... objs) {
+		System.out.printf(format, objs);
+		if (newLine)
+			println();
+	}
+}
