d998e6177f04a8522c9a4bf9e0befcf7eca5c93b
Alex C <agentleader1@gmail.com>
2021-03-29 00:37:55 -0700

    Created PhraseInString
    a program that determines the index of the n'th occurrence of a phrase in a string.
    
    does not account for many possibly user errors.

---
+++ src/com/alex/apcsa/PhraseInString.java
@@ -0,0 +1,71 @@
+package com.alex.apcsa;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PhraseInString {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		print("Insert String: ");
+		String str = reader.readLine();
+		print("Insert Phrase: ");
+		String phrase = reader.readLine();
+		int occ = -1;
+		do {
+			print("Insert Occurence: ");
+			try {
+				occ = Integer.parseInt(reader.readLine());
+				if (occ < 0) {
+					println("Invalid number, try again.");
+				} else {
+					break;
+				}
+			} catch (NumberFormatException ex) {
+				println("Not a number, try again.");
+			}
+			println();
+		} while (occ >= 0);
+		println("Index of the " + occ + "th occurence: " + indexOfOccurenceOfPhraseInString(0, occ, 0, str, phrase));
+		reader.close();
+	}
+
+	public static int indexOfOccurenceOfPhraseInString(int currentOcc, int finalOcc, int currentInd, String str,
+			String phrase) {
+		if (str.substring(currentInd, currentInd + phrase.length()).equalsIgnoreCase(phrase)) {
+			if (currentOcc + 1 == finalOcc) {
+				return currentInd;
+			} else {
+				return indexOfOccurenceOfPhraseInString(currentOcc + 1, finalOcc, currentInd + 1, str, phrase);
+			}
+		}
+		if (str.substring(currentInd).length() < phrase.length()) {
+			return -1;
+		} else {
+			return indexOfOccurenceOfPhraseInString(currentOcc, finalOcc, currentInd + 1, str, phrase);
+		}
+	}
+
+	public static void print(Object... objs) {
+		for (Object obj : objs) {
+			System.out.print(obj);
+		}
+	}
+
+	public static void println(Object... objs) {
+		if (objs.length <= 0) {
+			System.out.println();
+		} else {
+			for (Object obj : objs) {
+				System.out.println(obj);
+			}
+		}
+	}
+
+	public static void printf(boolean newLine, String format, Object... objs) {
+		System.out.printf(format, objs);
+		if (newLine)
+			println();
+	}
+}
